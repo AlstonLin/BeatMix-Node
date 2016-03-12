1634ffa8615e7862eb58796c7e760b9af3c7bbca
Alston Lin <alstonlin812@gmail.com>
2016-03-12 08:49:17 -0500

    Socket io test

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var express = require('express');
-var app = express();
-var PORT = 80;
+var express = require('express'),
+    fs = require('fs'),
+    http = require('http');
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    index = fs.readFileSync(__dirname + '/index.html'),
+    PORT = 80;
 
+var Session = require('./session');
+
+// Hello World at root to check if works
 app.get('/', function(req, res){
-    res.send('Hello World');
+  console.log("Requested");
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+});
+// Socket io
+setInterval(function(){
+    console.log("Updated");
+    io.emit('time', {
+      time: new Date().toJSON()
+    });
+  }, 10000);
+io.on('connection', function(socket) {
+    // Use socket to communicate with this particular client only, sending it it's own id
+    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+    socket.on('i am client', console.log);
+    console.log("Connection");
 });
 
-app.listen(PORT);
 console.log('Express server started on port %s', PORT);
+app.listen(PORT);
---
+++ session.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var sessionSchema = mongoose.Schema({
+});
+module.exports = mongoose.model('Session', sessionSchema);
