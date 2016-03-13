b6b9d94bc66525e72ca10e7f02191d27c585701f
Alston Lin <alstonlin812@gmail.com>
2016-03-13 04:55:36 -0400

    Added exploring songs

--- app.js
+++ app.js
@@ -20,22 +20,22 @@ app.get('/', function(req, res){
 io.on('connection', function(socket) {
     console.log("Connection");
     // Join a room
-    socket.on('join', function(data){
+    socket.on('join', function(data) {
       var code = data.code;
       console.log("Joined room " + code );
       socket.join(code);
     });
-    socket.on('command', function(data){
+    socket.on('command', function(data) {
       var roomName = Object.keys(socket.rooms)[1];
       io.in(roomName).emit('command', data);
       console.log("Command " + data.note + " at " + roomName);
     });
-    socket.on('end', function(data){
+    socket.on('end', function(data) {
       var roomName = Object.keys(socket.rooms)[1];
       io.in(roomName).emit('end', data);
       console.log("End at " + roomName);
     });
-    socket.on('addSong', function(data){
+    socket.on('addSong', function(data) {
       var title = data["title"];
       var author = data["author"];
       var content = JSON.stringify(data["content"]);
@@ -45,7 +45,7 @@ io.on('connection', function(socket) {
         author: author,
         content: content
       });
-      newSong.save(function(err){
+      newSong.save(function(err) {
         if (err){
           console.log("ERROR SAVING SONG: " + err);
         } else{
@@ -53,6 +53,24 @@ io.on('connection', function(socket) {
         }
       });
     })
+    socket.on('getSongs', function() {
+      var person = Object.keys(socket.rooms)[0];
+      var results = Song.find({}, { content: 0 }).sort( [['_id', -1]] ).limit(10);
+      console.log("Queried Songs: " + results);
+      io.in(person).emit('songs', results);
+    });
+    socket.on('getSong', function(data) {
+      var songId = data["songId"];
+      var result = Song.find(songId, { content: 1 });
+      console.log("Served Song id: " + songId);
+      io.in(person).emit('song', result);
+    });
+    socket.on('disconnect', function(){
+      var rooms = io.sockets.manager.roomClients[socket.id];
+      for(var room in rooms) {
+          socket.leave(room);
+      }
+    });
 });
 
 console.log('Express server started on port %s', PORT);
